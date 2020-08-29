ddc112f3a3872fa8cb82ec7dcbb3324a4fe6e4e2
jeanjik <45577016+jeanjik@users.noreply.github.com>
2020-08-29 12:17:41 +0200

    Update main.py
    messa altra robona

--- main.py
+++ main.py
@@ -19,6 +19,34 @@ class Network(object):
             activation = activation_function(np.dot(weights, activation) + biases)
             # np.dot e' una funzione che effettua la moltiplicazione matriciale
 
+    def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        # training_data e' una lista di tuple (x, y) dove x e' l'input e y e' la corrispondente label
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k + mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)
+                # ritorna un range che parte da 0 e arriva a n, con step mini_batch_size
+            ]
+            for mini_batch in mini_batches:
+                print("cazzo")
+
+
+    def update_mini_batches(self, mini_batch,eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases] # shape ritorna le dimensioni della matrice
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = "cazzo"  # TODO: qua ci va la backpropagation
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta / len(mini_batch)) * nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
 
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
