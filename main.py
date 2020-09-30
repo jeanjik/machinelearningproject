c0544ecdca7981310557d0ffb48599ffd057a88a
jeanjik <45577016+jeanjik@users.noreply.github.com>
2020-09-30 12:59:47 +0200

    Update main.py
    tutto

--- main.py
+++ main.py
@@ -7,62 +7,35 @@ class Network(object):
         #passa in input il num di neuroni di ogni layer: array[input, L1, L2, ...]
         self.num_layers = len(sizes)
         self.sizes = sizes
-        #self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.biases = [np.random.randn(1, y) for y in sizes[1:]] #ARRAY DI: matrice riga 1xL
-        # randn ritorna una matrice di dimensione y, 1 - ovvero una matrice colonna il cui numero di righe e' pari
-        # al numero di nodi nello strato corrente (escluso lo strato di input)
-
+        self.biases = [np.random.randn(1, y) for y in sizes[1:]]  # ARRAY DI: matrice riga 1xL
         self.weights = [np.random.randn(x, y) for x, y in zip(sizes[1:], sizes[:-1])]
         #ARRAY Per ogni layer L: LxL-1 (quando L=1 rappresentiamo la size dell'input)
-        #self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-        # i biases e weights sono salvati come liste di matrici per cui - ad esempio - weights[1] sara' una matrice
-        # contenente i pesi che connettono il secondo e terzo strato di neuroni
 
-    def forwardPropagation(self, activation, activation_function):
-        #for biases, weights in zip(self.biases, self.weights):
-            #activation = activation_function(np.dot(weights, activation) + biases)
-        # np.dot e' una funzione che effettua la moltiplicazione matriciale
+    def forward_propagation(self, input, activation_function):
+        # activation sono i valori di input per i nodi (per il primo strato sono proprio gli input x)
+        activation = input
+        activation_array = [input.T]
+        for weights, biases, layer in zip(self.weights, self.biases, range(0, self.num_layers)):
+            # weights: LxL-1 , activation: L-1x1 , w * a : Lx1 , biases.T : Lx1
+            a = np.dot(weights, activation) + biases.transpose()
+            # activation_array[layer][:] = a.T
+            activation_array.append(a.T)
+            activation = activation_function(a) # activation: Lx1 (in questo caso e' cio' che noi chiamiamo z
 
-        activation_array = [np.zeros(b.shape) for b in self.biases]
-        for weights, biases, x in zip(self.weights, self.biases, range(0, self.num_layers)):
-            #weights: LxL-1 , activation: L-1x1 , w * a : Lx1 , biases.T : Lx1
-            a=np.dot(weights, activation) + biases.transpose()
-            activation_array[x][:]=a.T
-            activation = activation_function(a)
-            #activation: Lx1
         return activation, activation_array
 
-    def backPropagation(self,activate,output,target,activation_function):
-        #creiamo la matrice delta errore
-        delta_err=[np.zeros(b.shape) for b in self.biases]
-        #dobbiamo distinguere due matrici: delta_out e delta_hidden?
-        #nodi output
-        error=output-target
-        delta_err[-1]= error.T * activation_function(activate[-1])
-        #nodi interni
-        for l in range(2,self.num_layers):
-            errore=np.dot(delta_err[-l+1],self.weights[-l+1])
-            delta_err[-l]= errore * activation_function(activate[-l])
+    def back_propagation(self, activate, output, target, activation_function, derivative_activation_function,
+                         derivative_cost_function):
+        delta_err_b = [np.zeros(b.shape) for b in self.biases]  # creiamo la matrice delta errore
+        derivative_err_w = [np.zeros(w.shape) for w in self.weights]
+        error = derivative_cost_function(output=output, target=target) # nodi output
+        delta_err_b[-1] = error.T * derivative_activation_function(activate[-1]) # caso base formula
+        derivative_err_w[-1] = np.dot(delta_err_b[-1].T, activation_function(activate[-2]))
+        for l in xrange(2, self.num_layers): # nodi interni
+            delta_err_b[-l] = np.dot(delta_err_b[-l+1], self.weights[-l+1]) * activation_function(activate[-l]) # formula ricorrente per i nodi interni
+            derivative_err_w[-l] = np.dot(delta_err_b[-l].T, activation_function(activate[-l-1]))
+        return delta_err_b, derivative_err_w
 
-        #come calcolo le derivate??
-        return delta_err
-
-    def backPropagationSecondaVersione(self,activate,output,target):
-        #PROVA
-        prova=[np.zeros(b.shape) for b in self.biases]
-        for k in range(output.size):
-        # Error = (Output value - Target value)
-           error = output[k] - target[k]
-           prova[-1][:,k] = error * dsigmoid(activate[-1][:, k])
-
-        for l in range(2,self.num_layers):
-            x = 0.0
-            for k in range(output.size):
-                x +=  prova[-l+1][:,k] * self.weights[-l+1][k]
-             # now, change in node weight is given by dsigmoid() of activation of each hidden node times the error.
-            prova[-l] = x * dsigmoid(activate[-l])
-
-        return prova
 
     def stochastic_gradient_descent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         # training_data e' una lista di tuple (x, y) dove x e' l'input e y e' la corrispondente label
@@ -93,36 +66,48 @@ class Network(object):
                        for b, nb in zip(self.biases, nabla_b)]
 
 
+def derivative_least_square(output, target):
+    return output - target
+
+
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
-def dsigmoid(z):
+
+def identity(z):
+    return z
+
+
+def derivative_sigmoid(z):
     return z * (1 - z)
 
-#print('---INSERISCI IL NUMERO DI STRATI DELLA RETE---')
-#print('---INSERISCI LA FUNZIONE DI OUTPUT---')
-#val=input()
-val=Network([5, 4, 4, 3])
-print('----RETE---')
+
+val = Network([4, 3, 2])
 print('----PESI---')
 print(val.weights)
 print('----BIASES---')
 print(val.biases)
-input = np.random.randn(1,5)
+input = np.random.randn(4, 1)
 print('----INPUT---')
 print(input)
 print('----APPLICO FORWARD PROPAGATION---')
-output,activation=val.forwardPropagation(input.transpose(),sigmoid)
+output,activation=val.forward_propagation(input,sigmoid)
 print('----OUTPUT---')
 print(output)
-expected=np.array([[0],[1],[0]])
+print('---ACTIVATION---')
+print(activation)
+
+expected = np.array([[0], [1]])
 print('----TARGET---')
 print(expected)
 print('----APPLICO BACK PROPAGATION---')
-delta=val.backPropagation(activation,output,expected,dsigmoid)
+delta, der_err = val.back_propagation(activate=activation, output=output, target=expected, activation_function=sigmoid,
+                             derivative_activation_function=derivative_sigmoid, derivative_cost_function=derivative_least_square)
 print 'DELTA:'
 print delta
-print val.backPropagationSecondaVersione(activation,output,expected)
+print 'DERIVATE ERRORE: '
+print der_err
+
 
 
 
