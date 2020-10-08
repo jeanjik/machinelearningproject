04ba96b594b116763c58fea9770d7c96ff93423c
jeanjik <45577016+jeanjik@users.noreply.github.com>
2020-10-08 20:34:08 +0200

    a me nun me piace a nutell

--- functions.py
+++ functions.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def derivative_least_square(output, target):
-    return output - target
+    return np.array([o - t for o, t in zip(output, target)])
 
 
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+def sigmoid(x):
+    return np.array([1.0 / (1.0 + np.exp(-z)) for z in x])
 
 
 def identity(z):
@@ -18,15 +18,25 @@ def least_square(output, target):
     return sum(err)*0.5
 
 
-def derivative_sigmoid(z):
-    return z * (1 - z)
+def derivative_sigmoid(x):
+    return np.array([z * (1 - z) for z in x])
 
 
 def softmax(output):
     return np.exp(output) / np.sum(np.exp(output))
 
 
+def derivative_cross_entropy(output, target):
+    output = softmax(output)
+    return (output - target) / (output * (1 - output))
+
+
+def derivative_identity(z):
+    return 1
+
+
 def cross_entropy(output, target):
+    output = softmax(output)
     err = [(t * np.log(o) + (1 - t) * np.log(1 - o)) for o, t in zip(output, target)]
     return - sum(err)
 
--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class Network(object):
         activation_array = [input.T]
         for weights, biases, layer in zip(self.weights, self.biases, range(0, self.num_layers)):
             # weights: LxL-1 , activation: L-1x1 , w * a : Lx1 , biases.T : Lx1
-            a = np.dot(weights, activation) + biases.transpose()
-            # activation_array[layer][:] = a.T
-            activation_array.append(a.T)
+            dot = np.dot(weights, activation)
+            a = np.array([d + b for d, b in zip(dot, biases.T)])
+            activation_array.append(a)
             activation = activation_function(a)  # activation: Lx1 (in questo caso e' cio' che noi chiamiamo z)
         return output_function(activation), activation_array
 
@@ -29,64 +29,72 @@ class Network(object):
                          derivative_cost_function=fn.derivative_least_square):
         delta_err_b = [np.zeros(b.shape) for b in self.biases]  # creiamo la matrice delta errore
         derivative_err_w = [np.zeros(w.shape) for w in self.weights]
-        error = derivative_cost_function(output=output, target=target)  # nodi output
-        delta_err_b[-1] = error.T * derivative_activation_function(activate[-1])  # caso base formula
-        derivative_err_w[-1] = np.dot(delta_err_b[-1].T, activation_function(activate[-2]))
+        der_error = derivative_cost_function(output=output, target=target)
+        act = derivative_activation_function(activate[-1])
+        delta_err_b[-1] = np.array(der_error * act).T  # nodi output
+        derivative_err_w[-1] = np.dot(delta_err_b[-1].T, activation_function(activate[-2]).T)
         for layer in xrange(2, self.num_layers):  # nodi interni
-            delta_err_b[-layer] = np.dot(delta_err_b[-layer + 1], self.weights[-layer + 1]) * activation_function(
-                activate[-layer])  # formula ricorrente per i nodi interni
-            derivative_err_w[-layer] = np.dot(delta_err_b[-layer].T, activation_function(activate[-layer - 1]))
+            temp = np.dot(delta_err_b[-layer + 1], self.weights[-layer + 1])
+            temp2 = activation_function(activate[-layer]).T
+            delta_err_b[-layer] = temp * temp2  # formula ricorrente per i nodi interni
+            temp3 = activation_function(activate[-layer - 1])[np.newaxis]
+            derivative_err_w[-layer] = np.dot(delta_err_b[-layer].T, temp3)
         return delta_err_b, derivative_err_w
 
-    def batch_gradient_descent(self, training_data, epochs, eta, momentum, test_data=None):
+    def batch_gradient_descent(self, training_data, epochs, eta, momentum):
         # training_data e' una lista di tuple (x, y) dove x e' l'input e y e' la corrispondente label
-        if test_data:
-            n_test = len(test_data)
-        n = len(training_data)
+        err = np.zeros(epochs)
         for j in xrange(epochs):
             random.shuffle(training_data)
             sum_w = [np.zeros(w.shape) for w in self.weights]
             sum_b = [np.zeros(b.shape) for b in self.biases]
-            for input, target in training_data:
+            for input, label in training_data:
                 output, activation = self.forward_propagation(input)
-                der_w, der_b = self.back_propagation(activation, output, target)
+                der_b, der_w = self.back_propagation(activation, output, label)
+                #print(der_w, der_b)
                 sum_b = [sb + derb for sb, derb in zip(sum_b, der_b)]
                 sum_w = [sw + derw for sw, derw in zip(sum_w, der_w)]
             self.weights = [(w - eta/len(training_data)) * momentum * wl for w, wl in zip(self.weights, sum_w)]
             self.biases = [(b - eta / len(training_data)) * momentum * bl for b, bl in zip(self.biases, sum_b)]
+            print(j)
+            err[j] = self.evaluate_error(training_data, fn.least_square)
+        return err
 
-
-    def evaluate_error(self, input, target, error_function):
-        output, _ = self.forward_propagation(input)
-        print(error_function(output, target))
+    def evaluate_error(self, training_data, error_function):
+        err = 0
+        for input, target in training_data:
+            output, _ = self.forward_propagation(input)
+            err = err + error_function(output, target)
+        return err
 
 
 
 def main():
-    val = Network([4, 3, 2, 2])
-    print('----PESI---')
-    print(val.weights)
-    print('----BIASES---')
-    print(val.biases)
-    input = np.random.randn(4, 1)
-    print('----INPUT---')
-    print(input)
-    print('----APPLICO FORWARD PROPAGATION---')
-    output, activation = val.forward_propagation(input)
-    print('----OUTPUT---')
-    print(output)
-    print('---ACTIVATION---')
-    print(activation)
+    val = Network([784, 200, 10])
+    input = np.random.rand(200, 784)
+    target = np.random.rand(200, 10)
+    training_data = [(i, t) for i, t in zip(input, target)]
 
-    expected = np.array([[0], [1]])
-    print('----TARGET---')
-    print(expected)
-    print('----APPLICO BACK PROPAGATION---')
-    delta, der_err = val.back_propagation(activate=activation, output=output, target=expected)
-    print 'DELTA:'
-    print delta
-    print 'DERIVATE ERRORE: '
-    print der_err
+    # print('----INPUT---')
+    # print(input)
+    # print('----APPLICO FORWARD PROPAGATION---')
+    # output, activation = val.forward_propagation(input)
+    # print('----OUTPUT---')
+    # print(output)
+    # print('---ACTIVATION---')
+    # print(activation)
+    #
+    # expected = target
+    # print('----TARGET---')
+    # print(expected)
+    # print('----APPLICO BACK PROPAGATION---')
+    # delta, der_err = val.back_propagation(activate=activation, output=output, target=expected)
+    # print 'DELTA:'
+    # print delta
+    # print 'DERIVATE ERRORE: '
+    # print der_err
+    err = val.batch_gradient_descent(training_data, 3, 0.1, 0.01)
+    print(err)
 
 
 main()
