63625c05a9bcce40d7a14a0504e6ef62e36cdf45
jeanjik <45577016+jeanjik@users.noreply.github.com>
2020-10-08 22:45:09 +0200

    adesso i dati li ritorna bene

--- loader.py
+++ loader.py
@@ -2,18 +2,32 @@ import gzip
 import numpy as np
 import matplotlib.pyplot as plt
 
-with gzip.open('./trainingdata/training_set_images.gz', 'r') as file:
-    buf = file.read(16)
-    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    image_size = 784
-    buf = file.read(3*image_size) # letto un'immagine intera
-    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    data = data.reshape(3, 28, 28, 1)
-    image = np.asarray(data[0]).squeeze()
-    plt.imshow(image)
-    plt.show()
-    lab = gzip.open('./trainingdata/trainin_set_labels.gz')
-    buf2 = lab.read(8)
-    buf2 = lab.read(1)
-    label = np.frombuffer(buf2, dtype=np.uint8).astype(np.int64)
-    print(label)
+IMAGE_SIZE = 784
+
+
+def load_data(path, num_images):
+    with gzip.open('./trainingdata/training_set_images.gz', 'r') as file_training_set:
+        buffer = file_training_set.read(16)
+        training_set = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+        buffer = file_training_set.read(num_images*IMAGE_SIZE) # letto un'immagine intera
+        training_set = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+        training_set = training_set.reshape(num_images, IMAGE_SIZE)
+    with gzip.open('./trainingdata/trainin_set_labels.gz') as file_labels:
+        buf2 = file_labels.read(8)
+        buf2 = file_labels.read(num_images)
+        lab_temp = np.frombuffer(buf2, dtype=np.uint8).astype(np.int64)
+        labels = [vectorized_result(lab) for lab in lab_temp]
+    data = [(input, target) for input, target in zip(training_set, labels)]
+    return data
+
+def vectorized_result(index):
+    vector = np.zeros(10)
+    vector[index] = 1
+    return vector
+
+
+
+nn = load_data(0, 3)
+for input, target in nn:
+    print(input)
+    print(target)
